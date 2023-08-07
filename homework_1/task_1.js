408cc569adcbee21a0fb6f02ea83237c55e436cb
nataliamoiseenko <natallia.moiseenko@gmail.com>
2023-08-07 11:24:54 +0300

    fix: console.log method

--- homework_1/task_1.js
+++ homework_1/task_1.js
@@ -1,11 +1,11 @@
 const firstValue = prompt('Enter first number', '');
 
 if (!firstValue) {
-  alert('Enter first number!');
+  console.log('Enter first number!');
 } else {
   const secondValue = prompt('Enter second number', '');
   if (!secondValue) {
-    alert('Enter second number!');
+    console.log('Enter second number!');
   } else {
     handleValues(+firstValue, +secondValue);
   }
@@ -13,14 +13,14 @@ if (!firstValue) {
 
 function handleValues(first, second) {
   if (isNaN(first) || isNaN(second)) {
-    alert('Incorrect input');
+    console.log('Incorrect input');
     return;
   }
 
   if (second % 1 !== 0) {
-    alert('Second number needs to be a whole integer');
+    console.log('Second number needs to be a whole integer');
     return;
   }
 
-  alert(first.toString(second));
+  console.log(first.toString(second));
 }
